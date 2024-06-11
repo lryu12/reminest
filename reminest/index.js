8e495bc0be2d2997c4511e53f46c8fe70078908b
louisryu <lryu@student.ubc.ca>
2024-06-11 11:40:21 -0700

    implemented hover effect using js

--- reminest/index.js
+++ reminest/index.js
@@ -1,1 +1,25 @@
-document.alert("bruhmoment");
+
+
+
+for (var i = 0; i<3; i++) {
+    document.querySelectorAll(".prev-emotions")[i].addEventListener("click", handleClick);
+}
+function handleClick() {
+    alert("I got clicked!");
+}
+
+svgLength = document.querySelectorAll("svg").length;
+for (var i = 0; i < svgLength; i++) {
+    var emoji = document.querySelectorAll("svg")[i];
+
+    emoji.addEventListener("mouseenter", function () {
+        this.style.color = "white";
+    })
+    
+    emoji.addEventListener("mouseleave", function () {
+        this.style.color = "#FFF9D0";
+    })
+}
+
+
+
