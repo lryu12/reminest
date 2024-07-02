2a2bf24edc909f1080b149c60c1b6fc7d71b62a0
lryu04 <ryulouis8@gmail.com>
2024-07-01 18:18:56 -0700

    color remains when clicked on emoji

--- reminest/index.js
+++ reminest/index.js
@@ -17,9 +17,32 @@ for (var i = 0; i < svgLength; i++) {
     })
     
     emoji.addEventListener("mouseleave", function () {
-        this.style.color = "#FFF9D0";
+        if (!this.classList.contains("clicked")) {
+            this.style.color = "#FFF9D0";
+        }
     })
+
+    emoji.addEventListener("click", function() {
+        this.classList.toggle('clicked');
+        if (this.classList.contains('clicked')) {
+            this.style.color = "red"; // Or whatever color you want when clicked
+        } else {
+            this.style.color = "#FFF9D0"; // Reset to the original color if unclicked
+        }
+    });
 }
+
+
+
+
+
+
+// function for submitting daily log
+// every part has to be filled or error message pops out
+
+// document.querySelector(".submit-button").addEventListener("click", function() {
+//     if ()
+// })
 
 
 
