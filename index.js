6f092084ee56aeb910248eb111058a789b7d2b42
lryu04 <ryulouis8@gmail.com>
2024-07-20 04:23:06 -0700

    connect postgres db to server

--- index.js
+++ index.js
@@ -2,11 +2,25 @@ import express from "express";
 import { dirname, join } from "path";
 import { fileURLToPath } from "url";
 import bodyParser from "body-parser";
+import pg from "pg";
+import { hostname } from "os";
 
 const app = express();
 const port = 3000;
 const __dirname = dirname(fileURLToPath(import.meta.url));
+
 app.use(bodyParser.urlencoded({ extended: true }));
+
+const db = new pg.Client({
+    user: "Louis",
+    hostname: "localhost",
+    password: "reminest",
+    port: 5432,
+    database: "reminest"
+});
+db.connect();
+
+
 
 var keyMoments;
 var newGoals;
@@ -28,11 +42,23 @@ app.get("/past-emotions", (req, res) => {
     res.render("past-emotions.ejs");
 });
 
-app.post("/submit", (req, res) => {
+
+
+app.post("/submit", async (req, res) => {
     keyMoments  = req.body['key-moments'];
     newGoals = req.body['new-goals'];
+
     console.log(`Key Moments: ${keyMoments}, New Goals: ${newGoals}`);
-    res.render("goals2achieve.ejs", { submittedData: {newGoals}});
+
+    await db.query(
+        "INSERT INTO daily_log (date, emotion, event) VALUES ($1, $2, $3)",
+        [new Date().toISOString().split('T')[0], "happy-test", keyMoments]
+    );
+
+      
+
+     res.render("goals2achieve.ejs", { submittedData: {newGoals}});
+
 });
 
 
